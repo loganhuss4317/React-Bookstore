aa1af78342bb611e15ce08a69a686064b7bccac2
loganhuss4317 <loganhuss@gmail.com>
2026-02-04 17:19:45 -0500

    new UI components

---
+++ src/components/Explore.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Explore = () => {
+  return (
+    <section id="explore">
+        <div className="container">
+            <div className="row row__column">
+                <h2>
+                    Explore more <span className="purple">Books</span>
+                </h2>
+                <a href="/books">
+                </a>
+                <button className="btn">Explore books</button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Explore;
---
+++ src/components/Featured.jsx
@@ -0,0 +1,28 @@
+import Book from './ui/Book';
+import React from 'react';
+import { books } from '../data.js';
+
+const Featured = () => {
+  
+  return (
+    <section id="featured">
+        <div className="container">
+            <div className="row">
+                <h2 className="section__title">
+                    Featured <span className="purple">Books</span>
+                </h2>
+                <div className="books">
+                    {books
+                    .filter((book) => book.rating === 5)
+                    .slice(0,4)
+                    .map((book) => (
+                        <Book book={book} key={book.id}/>)
+                    )}
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Featured;
