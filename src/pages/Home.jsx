aaf4c550ed0a9e03b9f9f57900d2eea3720454ef
loganhuss4317 <loganhuss@gmail.com>
2026-02-06 15:58:41 -0500

    books.jsx update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,21 @@
-import React from "react";
-import Landing from "../components/Landing";
-import Highlights from "../components/Highlights";
-import Features from "../components/Featured";
-import CheapestBooks from "../components/CheapestBooks";
-import Explore from "../components/Explore";
+import React from 'react';
+import Landing from '../components/Landing';
+import Highlights from '../components/Highlights';
+import Featured from '../components/Featured';
+import Discounted from '../components/Discounted';
+import Explore from '../components/Explore';
 
 const Home = () => {
   return (
-    <>
-      <Landing />
-      <main>
-        <Highlights />
-        <Features />
-        <CheapestBooks />
-        <Explore />
-      </main>
-    </>
+    <><Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+      </>
   );
 };
 
 export default Home;
+
+
