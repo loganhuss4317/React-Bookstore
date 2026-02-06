8697219837f0a835ce3aceefe101e95a48072366
loganhuss4317 <loganhuss@gmail.com>
2026-02-06 18:21:28 -0500

    filters added

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Book from '../components/ui/Book';
 
-const Books = ({ books }) => {
-  return (
+const Books = ({ books: initialBooks }) => {
+  const [books, setBooks] = useState(initialBooks);
+
+  function filterBooks(filter) {
+    console.log(filter)
+    if (filter === 'LOW_TO_HIGH') {
+        setBooks(books.slice().sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)));
+    }
+    if (filter === 'HIGH_TO_LOW') {
+        setBooks(books.slice().sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)));
+    }
+    if (filter === "RATING") {
+        setBooks(books.slice().sort((a, b) => b.rating - a.rating));
+    }
+  }
+  
+    return (
     <div id="books__body">
         <main id="books__main">
             <section>
@@ -11,7 +26,7 @@ const Books = ({ books }) => {
                         <div className="books__header">
                             <h2 className='section__title books__header--title'>
                                 All Books</h2>
-                                <select id="filter" defaultValue="DEFAULT">
+                                <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)}>
                                     <option value="DEFAULT" disabled>Sort</option>
                                     <option value="LOW_TO_HIGH">Price, Low to High</option>
                                     <option value="HIGH_TO_LOW">Price, High to Low</option>
