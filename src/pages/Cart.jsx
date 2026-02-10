77f9254fbe60b7f771c71e1416f7892244729343
loganhuss4317 <loganhuss@gmail.com>
2026-02-10 12:52:03 -0500

    add price and cart components

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <div id="books__body">
+        <main id="books__main">
+            <div className="books__container">
+                <div className="row">
+                    <div className="book__selected--top">
+                        <h2 className="cart__title">
+                            Cart
+                        </h2>
+                    </div>
+                    <div className="cart">
+                        <div className="cart__header">
+                            <div className="cart__book">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+  )
+}
+
+export default Cart;
